cabdc0d34056078ad21c6915d32c363c332e7f28
Lonely Dream <Zhenhui.Cai@foxmail>
2024-11-18 09:23:02 +0800

    feat: 完成初版

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # naming-style README
 
-This is the README for your extension "naming-style". After writing up a brief description, we recommend including the following sections.
+这是一个在VS Code中将选中文本按照指定命名风格进行重命名的扩展。
 
 ## Features
 
@@ -8,7 +8,7 @@ Describe specific features of your extension including screenshots of your exten
 
 For example if there is an image subfolder under your extension project workspace:
 
-\!\[feature X\]\(images/feature-x.png\)
+![demo](images/try_demo.gif)
 
 > Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
 
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,69 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+/**
+ * 根据不同的命名风格分割为全小写字符串数组
+ *
+ * @param identifier 待分割的字符串
+ * @returns 分割后的字符串数组
+ */
+function splitIdentifier(identifier: string): string[] {
+	if (!identifier) {
+		return [""];
+	}
+
+	// 根据不同的命名风格分割字符串
+	if (/^[A-Z][a-zA-Z]*$/.test(identifier)) {
+		// VarBigHump: 大驼峰
+		return identifier.split(/(?=[A-Z])/).map(word => word.toLowerCase());
+	} else if (/^[a-z][a-zA-Z]*$/.test(identifier)) {
+		// varLittleHump: 小驼峰
+		return identifier.split(/(?=[A-Z])/).map(word => word.toLowerCase());
+	} else if (/^[a-z]+(_[a-z]+)*$/.test(identifier)) {
+		// var_snake_case: 蛇形命名
+		return identifier.split('_');
+	} else if (/^[A-Z]+(_[A-Z]+)*$/.test(identifier)) {
+		// VAR_CONSTANT_CASE: 常量命名
+		return identifier.split('_').map(word => word.toLowerCase());
+	} else {
+		// 其他不支持的命名风格 返回原字符串
+		return [identifier];
+	}
+}
+
+
+function convertToBigHump(identifier: string): string {
+	const words = splitIdentifier(identifier);
+	if (words.length === 1) {
+		return words[0];
+	}
+	return words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join("");
+}
+
+function convertToLittleHump(identifier: string): string {
+	const words = splitIdentifier(identifier);
+	if (words.length === 1) {
+		return words[0];
+	}
+	var newIdentifier = words.map((word, index) => {
+		if (index === 0) {
+			return word.charAt(0).toLowerCase() + word.slice(1);
+		}
+		return word.charAt(0).toUpperCase() + word.slice(1);
+	}).join("");
+	return newIdentifier;
+}
+
+function convertToSnakeCase(identifier: string): string {
+	const words = splitIdentifier(identifier);
+	return words.join("_");
+}
+
+function convertToConstantCase(identifier: string): string {
+	const words = splitIdentifier(identifier);
+	return words.map(word => word.toUpperCase()).join("_");
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -10,16 +73,66 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "naming-style" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('naming-style.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Naming Style!');
-	});
+	const disposable_list = [
+		vscode.commands.registerCommand('naming-style.convertToBigHump', () => {
+			const editor = vscode.window.activeTextEditor;
+			if (!editor) {
+				return;
+			}
+			const selections = editor.selections;
+			const text = editor.document.getText(selections[0]);
+			const newText = convertToBigHump(text);
+			editor.edit(editBuilder => {
+				selections.forEach(selection => {
+					editBuilder.replace(selection, newText);
+				});
+			});
+		}),
+		vscode.commands.registerCommand('naming-style.convertToLittleHump', () => {
+			const editor = vscode.window.activeTextEditor;
+			if (!editor) {
+				return;
+			}
+			const selections = editor.selections;
+			const text = editor.document.getText(selections[0]);
+			const newText = convertToLittleHump(text);
+			editor.edit(editBuilder => {
+				selections.forEach(selection => {
+					editBuilder.replace(selection, newText);
+				});
+			});
+		}),
+		vscode.commands.registerCommand('naming-style.convertToSnakeCase', () => {
+			const editor = vscode.window.activeTextEditor;
+			if (!editor) {
+				return;
+			}
+			const selections = editor.selections;
+			const text = editor.document.getText(selections[0]);
+			const newText = convertToSnakeCase(text);
+			editor.edit(editBuilder => {
+				selections.forEach(selection => {
+					editBuilder.replace(selection, newText);
+				});
+			});
+		}),
+		vscode.commands.registerCommand('naming-style.convertToConstantCase', () => {
+			const editor = vscode.window.activeTextEditor;
+			if (!editor) {
+				return;
+			}
+			const selections = editor.selections;
+			const text = editor.document.getText(selections[0]);
+			const newText = convertToConstantCase(text);
+			editor.edit(editBuilder => {
+				selections.forEach(selection => {
+					editBuilder.replace(selection, newText);
+				});
+			});
+		})
+	];
 
-	context.subscriptions.push(disposable);
+	disposable_list.forEach(disposable => { context.subscriptions.push(disposable); });
 }
 
 // This method is called when your extension is deactivated
